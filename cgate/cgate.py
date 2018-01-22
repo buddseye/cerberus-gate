0bb47ea0e18e9015a5e307598737a6f7d994ac0b
buddseye <hokurodaibutu@live.jp>
2018-01-22 13:57:41 +0900

    add command option --null.

--- cgate/cgate.py
+++ cgate/cgate.py
@@ -10,6 +10,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('target', help='Table name or File path')
     parser.add_argument('--schema', '-s', help='Cerberus schema file')
+    parser.add_argument('--null', '-n', help='Null character', default='NULL,\\N')
     args = parser.parse_args()
 
     schema = readschema(args.schema)
@@ -17,8 +18,9 @@ def main():
         header = schema['header']
     except:
         header = None
+    na_values = args.null.split(',')
     dtype, date_cols = get_dtype(schema['schema'])
-    dfs = readfile(args.target, header=header, dtype=dtype, parse_dates=date_cols)
+    dfs = readfile(args.target, header=header, dtype=dtype, parse_dates=date_cols, na_values=na_values)
     fail_count = validate(dfs, schema['schema'])
     if fail_count != 0:
         print('Failed {0} error...'.format(fail_count), file=sys.stderr)
